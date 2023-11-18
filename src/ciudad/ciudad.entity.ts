bcc1afd0df2523af7834e9a47973971f1c08690d
andres.cantor@payu.com <andres.cantor@payu.com>
2023-11-17 23:20:34 -0500

    entidades

---
+++ src/ciudad/ciudad.entity.ts
@@ -0,0 +1,21 @@
+import { SupermercadoEntity } from "src/supermercado/supermercado.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class CiudadEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    pais: string;
+
+    @Column()
+    numeroDeHabitantes: number;
+
+    @ManyToMany(() => SupermercadoEntity, supermercado => supermercado.sedes)
+    supermercados: SupermercadoEntity[];
+
+}
