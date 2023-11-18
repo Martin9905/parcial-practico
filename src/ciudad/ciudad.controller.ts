e6570bf3c3287fcffa5a4119bea3b04c6c739801
andres.cantor@payu.com <andres.cantor@payu.com>
2023-11-18 18:15:36 -0500

    adicion de controladores

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,10 +1,41 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { CiudadService } from './ciudad.service';
 import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+import { CiudadEntity } from './ciudad.entity';
+import { CiudadDto } from './ciudad.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('cities')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CiudadController {
     constructor(private readonly ciudadService: CiudadService){}
+
+    @Get()
+    async findAll() {
+        return await this.ciudadService.findAll();
+    }
+
+    @Get(':cityId')
+    async findOne(@Param('cityId') cityId: string) {
+        return await this.ciudadService.findOne(cityId);
+    }
+    
+    @Post()
+    async create(@Body() cityDto: CiudadDto) {
+        const ciudad: CiudadEntity = plainToInstance(CiudadEntity, cityDto);
+        return await this.ciudadService.create(ciudad);
+    }
+
+    @Put(':cityId')
+    async update(@Param('cityId') cityId: string, @Body() cityDto: CiudadDto) {
+        const ciudad: CiudadEntity = plainToInstance(CiudadEntity, cityDto);
+        return await this.ciudadService.update(cityId, ciudad);
+    }
+
+    @Delete(':cityId')
+    @HttpCode(204)
+    async delete(@Param('cityId') cityId: string) {
+        return await this.ciudadService.delete(cityId);
+    }
 
 }
--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -1,10 +1,41 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { SupermercadoService } from './supermercado.service';
 import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+import { SupermercadoDto } from './supermercado.dto';
+import { SupermercadoEntity } from './supermercado.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('supermarkets')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class SupermercadoController {
     constructor(private readonly supermercadoService: SupermercadoService){}
+
+    @Get()
+    async findAll() {
+        return await this.supermercadoService.findAll();
+    }
+
+    @Get(':supermarkedId')
+    async findOne(@Param('supermarkedId') supermarkedId: string) {
+        return await this.supermercadoService.findOne(supermarkedId);
+    }
+    
+    @Post()
+    async create(@Body() supermarketDto: SupermercadoDto) {
+        const supermercado: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
+        return await this.supermercadoService.create(supermercado);
+    }
+
+    @Put(':supermarkedId')
+    async update(@Param('supermarkedId') supermarkedId: string, @Body() supermarketDto: SupermercadoDto) {
+        const supermercado: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
+        return await this.supermercadoService.update(supermarkedId, supermercado);
+    }
+
+    @Delete(':supermarkedId')
+    @HttpCode(204)
+    async delete(@Param('supermarkedId') supermarkedId: string) {
+        return await this.supermercadoService.delete(supermarkedId);
+    }
 
 }
