01607329a5934e2458bab038a0e307766fb38b2f
andres.cantor@payu.com <andres.cantor@payu.com>
2023-11-18 12:11:56 -0500

    preparacion para las pruebas unitarias de los servicios

--- src/supermercado/supermercado.service.spec.ts
+++ src/supermercado/supermercado.service.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupermercadoService } from './supermercado.service';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SupermercadoEntity } from './supermercado.entity';
 
 describe('SupermercadoService', () => {
   let service: SupermercadoService;
+  let repository: Repository<SupermercadoEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [SupermercadoService],
     }).compile();
 
     service = module.get<SupermercadoService>(SupermercadoService);
+    repository = module.get<Repository<SupermercadoEntity>>(getRepositoryToken(SupermercadoEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+
+  
 });
